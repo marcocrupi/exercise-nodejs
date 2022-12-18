55471c632c4edb8aa91d9239e69800e46416050d
marcocrupi <marcocrupi@hotmail.it>
2022-12-18 09:34:12 +0100

    exercise-nodejs-03

--- exercise-nodejs-03-script.js
+++
@@ -1,8 +0,0 @@
-// Exercise 3 - Create and use CommonJS modules
-
-// Create a script that uses module.exports to export a function.
-// Create another script that uses require() to import the function and then calls it.
-
-import outputMessage from "./exercise-nodejs-03-output-message";
-
-outputMessage("Hey there!");
---
+++ exercise-nodejs-03-script.mjs
@@ -0,0 +1,8 @@
+// Exercise 3 - Create and use CommonJS modules
+
+// Create a script that uses module.exports to export a function.
+// Create another script that uses require() to import the function and then calls it.
+
+import outputMessage from "./exercise-nodejs-03-output-message.mjs";
+
+outputMessage("Hey there!");
